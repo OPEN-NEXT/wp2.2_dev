4ec253d9be10f121b6b6778e559560b3400f9f44
Pen-Yuan Hsing <penyuanhsing@posteo.is>
2020-05-06 15:35:00 +0100

    Add SPDX license idenfiers

--- src/get_Github_forks.py
+++ src/get_Github_forks.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 # Recursively find all forks of a Github repository
 
--- src/get_commits.py
+++ src/get_commits.py
@@ -1,7 +1,6 @@
-# [license info here]
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 # Get commits in a git repository.
 # When two files are edited within the same commit, that counts as a connection
