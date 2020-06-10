688f1c2cc737c2b829419afb69569bdcc6256e60
Pen-Yuan Hsing <penyuanhsing@posteo.is>
2020-06-10 15:10:53 +0100

    Add initialisation script

---
+++ src/initialise.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Gather parameters from commandline arguments and the configuration file to 
+# initialise the rest of the scripts in this repository.
+
