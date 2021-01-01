e58965df59a76a5579403f0712190c3c97505c78
Pen-Yuan Hsing <penyuanhsing@posteo.is>
2021-01-01 18:46:23 +0000

    Create __main__.py

---
+++ osdash/__main__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+"""
+This is the dashboard's main module.
+It defines a main function to be executed, representing execution of the
+whole package. This function is high-level and calls other internal modules
+that contain the actual plumbing.
+"""
+
+# Python Standard Library import(s)
+
+# External import(s)
+
+# Internal import(s)
+
+# High-level `main()` that spells out data-mining logic
+def main():
+    """
+    docstring
+    """
+    # Run pre-processor that reads list of repositories to mine and existing data
+    #staging_data = 
+
+if __name__ == "__main__":
+    main()
