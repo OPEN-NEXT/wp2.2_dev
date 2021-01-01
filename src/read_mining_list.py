a6d0764b9c0ecb6aca205782f0d64d28081b9a8d
Pen-Yuan Hsing <penyuanhsing@posteo.is>
2021-01-01 17:16:48 +0000

    Create function for reading repos list

--- src/read_mining_list.py
+++ src/read_mining_list.py
@@ -2,4 +2,19 @@
 # -*- coding: utf-8 -*-
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+# Import external modules
 import pandas
+
+def read_repo_list(path: str = "../data/OSH-repos.csv") -> pandas.core.frame.DataFrame:
+    """
+    docstring
+    """
+    repo_list: pandas.core.frame.DataFrame = pandas.read_csv(path)
+
+    # TODO: Basic input data validation happens here.
+
+    return repo_list
+
+my_repos = read_repo_list()
+
+print("pause")
