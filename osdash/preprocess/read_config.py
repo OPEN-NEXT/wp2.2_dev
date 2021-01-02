832a3f908a2cb10a7c99a1395c9b4b0fc1afc817
Pen-Yuan Hsing <penyuanhsing@posteo.is>
2021-01-02 01:19:46 +0000

    Initial implemention to read configuration

--- osdash/preprocess/read_config.py
+++ osdash/preprocess/read_config.py
@@ -2,18 +2,23 @@
 # -*- coding: utf-8 -*-
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-# 
+# TODO: Finish logging functionality
+
+# Python Standard Library imports
 import argparse
-import csv
 import logging
 import os
 import sys
-from logging.config import dictConfig
 
-#
+# External imports
 import yaml
 
-def read_config():
+# Default options
+DEFAULT_REPO_LIST: str = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "input")
+DEFAULT_DATA_DIR: str = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "data")
+
+# Function to process commandline and configuration file options
+def read_config() -> dict:
     """Initialise starting options for git-mining script
 
     Returns:
@@ -28,62 +33,85 @@ def read_config():
     # Retrive configuration options
     #
     print("Reading configuration")
+    print(os.path.abspath(__file__))
 
-    # Get commandlines options first
-    parser = argparse.ArgumentParser(
+    # Get commandline options first
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
         description="Mines metadata from GitHub repositories given a list.")
     parser.add_argument("-c", "--config_file", type=str, required=False,
                         help="Path to configuration file. Will override corresponding --auth and --repolist options.")
     parser.add_argument("-a", "--GitHub_token", type=str, required=False,
                         help="Path to GitHub personal authentication token file.")
-    parser.add_argument("-d", "--data_dir", type=str, default="__DATA__", required=False,
+    parser.add_argument("-d", "--data_dir", type=str, required=False,
                         help="Output directory for storing downloaded data.")
-    parser.add_argument("-r", "--repo_list", type=str, default="repolist_example.csv", required=False, 
+    parser.add_argument("-r", "--repo_list", type=str, required=False, 
                         help="Path to CSV file containing list of repositories to mine.")
     parser.add_argument("--create_data_dir", type=bool, default=True, required=False,
                         help = "If data output directory doesn't exist, create it.")
-    parsed_config = parser.parse_args()
+    parsed_config: argparse.Namespace = parser.parse_args()
 
     # Create dictionary to hold mandatory options
     configuration: dict = dict()
-    # Put user supplied options into the dictionary
+    configuration["repo_list"] = DEFAULT_REPO_LIST
+    configuration["GitHub_token"] = None
+    configuration["data_dir"] = DEFAULT_DATA_DIR
+    configuration["create_data_dir"] = None
+    # Put commandline options into the dictionary
     configuration["config_file"] = parsed_config.config_file
-    configuration["GitHub_token"] = parsed_config.GitHub_token
-    configuration["data_dir"] = parsed_config.data_dir
-    configuration["repo_list"] = parsed_config.repo_list
-    configuration["create_data_dir"] = parsed_config.create_data_dir
-    #configuration["log_config"] = None
 
     #
-    # Apply configuration file if it is supplied
+    # Apply configuration file first
     #
 
-    if (configuration["config_file"] == None) or (configuration["config_file"] == ""):
-        logging.info("No configuration file specified.")
-    else:
+    if not(configuration["config_file"] == None):
         logging.info("Trying to parse configuration file:" + configuration["config_file"])
         # Start an empty configuration then populate from file
-        custom_config = None
+        config_file_options = None
         try:
             with open(file=configuration["config_file"], mode="r") as config_file:
                 try:
-                    custom_config = yaml.safe_load(config_file)
-                    #custom_config = json.load(config_file)
+                    config_file_options = yaml.safe_load(config_file)
                 except yaml.YAMLError as yaml_error:
                     logging.critical(f"Error parsing configuration file: \n{yaml_error}")
                     sys.exit(1)
                 del config_file
         except FileNotFoundError as not_found:
-            logging.critical(f"Specified configuration file not found: {not_found}")
+            logging.critical(f"Configuration file not found: {not_found}")
         else:
-            # Override commandline options with configuration file
-            for key in custom_config.keys():
+            # Apply options from configuration file
+            for key in config_file_options.keys():
                 try:
                     configuration[key] # Will throw KeyError exception if option isn't defined
                 except KeyError as key_error: # Deal with undefined option
                     logging.warning(f"{key_error} option is not supported in configuration file.")
                 else:
-                    configuration[key] = custom_config[key] # Only valid options will be used propogated
+                    configuration[key] = config_file_options[key] # Only valid options will be used propogated
+    
+    #
+    # Read environment variables
+    #
+
+    # Read GitHub token from environment variable if it is there
+    # Reference: https://stackoverflow.com/q/40697845/186904
+    if "GITHUB_TOKEN" in os.environ:
+        logging.info(f"Reading GitHub token from environment variable GITHUB_TOKEN")
+        configuration["GitHub_token"] = os.environ["GITHUB_TOKEN"]
+
+    #
+    # Apply commandline arguments
+    #
+
+    # Commandline arguments would override anything in the configuration file
+    # or environment variables
+
+    if parsed_config.GitHub_token != None: 
+        configuration["GitHub_token"] = parsed_config.GitHub_token
+    if parsed_config.data_dir != None: 
+        configuration["data_dir"] = parsed_config.data_dir
+    if parsed_config.repo_list != None: 
+        configuration["repo_list"] = parsed_config.repo_list
+    if (parsed_config.create_data_dir != None) and (configuration["create_data_dir"] == None): 
+        configuration["create_data_dir"] = parsed_config.create_data_dir
 
 
     #
@@ -100,49 +128,39 @@ def read_config():
         sys.exit(1)
 
     #
-    # Handle log level option
-    #
-
-    # Apply user-supplied config, if supplied. If not, continue with default settings
-    # if "log_config" in configuration:
-    #     try:
-    #         dictConfig(configuration["log_config"])
-    #     except Exception as log_config_error:
-    #         logging.critical(f"Error parsing log config: {log_config_error}")
-    #         sys.exit(1)
-
-    #
     # Process GitHub API token
     #
 
     # Put the authentication string into its own entry
-    try:
-        with open(configuration["GitHub_token"], mode="r") as token_file:
-            token_file_lines = token_file.read().split(sep="\n")
-            # Read first line of provided token file as authentication token string
-            configuration["GitHub_token"] = token_file_lines[0]
-        del token_file, token_file_lines
-    except FileNotFoundError as token_file_error:
-        logging.critical(f"Can't find GitHub API authentication token file.")
-        logging.critical(token_file_error)
-        sys.exit(1)
-    except Exception as other_error:
-        logging.critical(f"Error accessing GitHub API authentication token file: {other_error}")
-        sys.exit(1)
-    else: 
-        # Check if authentication key string looks correct
-        # AFAIK the token should be exactly 40 alphanumeric characters
+    if not("GITHUB_TOKEN" in os.environ):
+        # I.e. Try to open token file if the token is not already read from environment
         try:
-            assert (configuration["GitHub_token"].isalnum() and len(configuration["GitHub_token"]) == 40)
-        except AssertionError:
-            logging.critical("GitHub authentication key doesn't look right: {}".format(configuration["GitHub_token"]))
-            logging.critical("It should be a 40-character alphanumeric string. Please try again.")
+            with open(configuration["GitHub_token"], mode="r") as token_file:
+                token_file_lines = token_file.read().split(sep="\n")
+                # Read first line of provided token file as authentication token string
+                configuration["GitHub_token"] = token_file_lines[0]
+            del token_file, token_file_lines
+        except FileNotFoundError as token_file_error:
+            logging.critical(f"Can't find GitHub API authentication token file.")
+            logging.critical(token_file_error)
             sys.exit(1)
-        else:
-            logging.debug("GitHub authentication key looks OK.")
+        except Exception as other_error:
+            logging.critical(f"Error accessing GitHub API authentication token file: {other_error}")
+            sys.exit(1)
+
+    # Check if GitHub authentication key string looks correct
+    # AFAIK the token should be exactly 40 alphanumeric characters
+    try:
+        assert (configuration["GitHub_token"].isalnum() and len(configuration["GitHub_token"]) == 40)
+    except AssertionError:
+        logging.critical("GitHub authentication key doesn't look right: {}".format(configuration["GitHub_token"]))
+        logging.critical("It should be a 40-character alphanumeric string. Please try again.")
+        sys.exit(1)
+    else:
+        logging.debug("GitHub authentication key looks OK.")
     
     #
-    # Create ouput data directory `data_dir`
+    # Prepare output data directory `data_dir`
     #
 
     # If `data_dir` doesn't exist, create it unless explicitly disabled
@@ -158,60 +176,18 @@ def read_config():
             logging.critical("Option 'create_data_dir' is {}, exiting.".format(configuration["create_data_dir"]))
             sys.exit(1)
 
-    #
-    # Process input list of repositories (`repo_list`) to mine
-    #
-
-    # Check if `repo_list` exists and parses correctly
-    try:
-        assert os.path.isfile(configuration["repo_list"]), "Error accessing repository list: {}".format(configuration["repo_list"])
-    except AssertionError as no_repo_list:
-        logging.critical(no_repo_list)
-        sys.exit(1)
-    else:
-        try:
-            with open(configuration["repo_list"], newline="") as repo_file:
-                repo_csv = csv.DictReader(repo_file)
-                repo_list: list = list()
-                for row in repo_csv:
-                    repo_list.append(row)
-            del repo_file, repo_csv
-        except Exception as read_csv_error: # TODO: Make Exception more specific
-            logging.critical(f"Error parsing repository list: {read_csv_error}")
-            sys.exit(1)
-    # Check repository list format
-    bad_rows: list = list() # Create an empty list to record list items in wrong format
-    for row in repo_list: # TODO: More efficient way to check than go through each list item
-        try: # Make sure "owner" and "repo" fields consistently exist
-            assert "owner" in row and "repo" in row, "Repository list CSV file needs field names 'owner' and 'row'"
-        except AssertionError as fieldname_error:
-            logging.critical(fieldname_error)
-            sys.exit(1)
-        else:
-            # Record which items don't have exactly two items, one each for "owner" and "repo", 
-            # plus if there are empty (None) cells
-            if len(row) != 2 or None in row.values():
-                bad_rows.append(row)
-    try: # If there are bad items in repository list, print them and exit
-        assert len(bad_rows) == 0, "Some rows in repository list have problems: "
-    except AssertionError as item_error:
-        logging.critical(item_error)
-        for bad_row in bad_rows:
-            logging.critical(bad_row.values()) # TODO: Make this more human-readable
-        sys.exit(1)
-            
-    # TODO: Remove duplicate entries in `repo_list`
-    # Put `repo_list` into `configuration`
-    configuration["repo_list"] = repo_list
-
     # Return a dictionary of initialisation options
     return configuration
 
+# If this file is run on its own, usually in a debugging context, then main() 
+# will run: 
 def main(): 
-    print("In read_config.py's __main__()")
-    current_path = os.getcwd()
-    print(current_path)
-    read_config()
+    print(f"In read_config.py's main()")
+    print(f"Current workding directory is: ")
+    print(os.getcwd())
+    config: dict = read_config()
+    print(f"main() done")
 
 if __name__ == "__main__":
     main()
+    sys.exit(0)
