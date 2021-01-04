f3ca0972d926671d64560ed9dcf35e69a4985e66
Pen-Yuan Hsing <penyuanhsing@posteo.is>
2021-01-04 02:55:58 +0000

    Create initial iteration over repo URLs

--- osdash/miner/adapters/GitHub.py
+++ osdash/miner/adapters/GitHub.py
@@ -12,6 +12,19 @@ def GitHub(repo_list: pandas.core.frame.DataFrame) -> pandas.core.frame.DataFram
     docstring
     """
     print(f"Begin GitHub adapter")
+
+    # Create empty DataFrame to hold mined data
+    mined_data: pandas.core.frame.DataFrame = repo_list[["repo_url", "last_mined"]]
+
+    # For each repository (row) in `repo_list`, mine data at its URL
+    # Use itertuples() because it seems to be much faster than iterrows()
+    # Reference: https://stackoverflow.com/a/10739432/186904
+    # TODO: Consider using Pandas's apply() instead: https://stackoverflow.com/a/30566899/186904
+    for repo in repo_list.itertuples(): 
+        # Since itertuples() returns data of namedtuple type, use getattr() to 
+        # access items in each row
+        # Reference: https://medium.com/@rinu.gour123/python-namedtuple-working-and-benefits-of-namedtuple-in-python-276d679b2e9c
+        print(f"Processing: " + getattr(repo, "repo_url"))
     mined_data: pandas.core.frame.DataFrame = repo_list
 
     return mined_data
