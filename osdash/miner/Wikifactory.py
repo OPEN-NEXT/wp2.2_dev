2891f55f1380a6be1bb5ec6122780499c508f520
Pen-Yuan Hsing <penyuanhsing@posteo.is>
2021-01-06 17:16:36 +0000

    Initial code for Wikifactory.py

--- osdash/miner/Wikifactory.py
+++ osdash/miner/Wikifactory.py
@@ -2,10 +2,47 @@
 # -*- coding: utf-8 -*-
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+"""[summary]
+Wikifactory API adapter/interface for data mining script
+"""
+
 # Python Standard Library imports
+import datetime
+import math
+import string
+import sys
+import time
+import urllib.parse
 
 # External imports
 import pandas
+import requests
+
+#
+# Define basic parameters for Wikifactory API
+#
+
+# Wikifactory GraphQL API endpoint
+GRAPHQL_URL: str = "https://wikifactory.com/api/graphql"
+# Wikifactory API query success response code
+SUCCESS_CODE: int = 200
+# Wikifactory API query response codes when retry will be attempted
+RETRY_CODES: list = [429, 500, 502, 503, 504]
+# Initial retry wait time in seconds
+RETRY_WAIT: int = 10
+# Numbers of times to retry before fail
+RETRIES: int = 5
+# Requested results per page for each API response
+PER_PAGE: int = 100
+# An arbitrarily early "last minted" timestamp if a repository has not been 
+# mined before
+DEFAULT_LAST_MINED: str = "1970-01-01T00:00:00.000000+00:00"
+
+# Define a custom exception for when queries fail repeatedly
+class WikifactoryAPIError(Exception): 
+    pass
+
+
 
 def Wikifactory(repo_list: pandas.core.frame.DataFrame) -> dict:
     """
