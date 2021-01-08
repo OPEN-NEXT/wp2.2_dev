f8837d4f4ab514b27fd48271ddaddce3ae464fbb
Pen-Yuan Hsing <penyuanhsing@posteo.is>
2021-01-08 21:05:48 +0000

    More complete implementation of Wikifactory queries

--- osdash/miner/Wikifactory.py
+++ osdash/miner/Wikifactory.py
@@ -90,7 +90,7 @@ def parse_url(url: str) -> dict:
     return repo
 
 # Get a Wikifactory project's metadata, contributions, and issues
-def get_project_data(repo: dict):
+def get_project_data(repo: dict) -> dict:
     """[summary]
     Using the Wikifactory GraphQL API
     Args:
@@ -98,12 +98,6 @@ def get_project_data(repo: dict):
     """
     # Fetch data
 
-    # Track if there is a next page of results
-    query_has_next_page: bool = True
-    # Track results page number
-    query_page: int = 1
-    # Create a pagination cursor
-    end_cursor: str = "null" # "null" because there is no cursor for first query
     # Initialise an empty list of contributions
     contributions: list = []
     # Initialise an empty list of issues
@@ -111,11 +105,20 @@ def get_project_data(repo: dict):
 
     # The main structure of the query, with space for `tracker` and 
     # `contributions` elements
-    main_query: string.Template = string.Template(
-    """
+    main_query: string.Template = string.Template("""
     {
-        project(space: $space, slug: $slug) {
+        project(space: "$space", slug: "$slug") {
             result {
+    $project_metadata
+    $contributions
+    $tracker
+            }
+        }
+    }
+    """
+    )
+    # Query fragment for project metadata
+    project_metadata_fragment: str = """
                 id
                 title
                 creator {
@@ -124,21 +127,17 @@ def get_project_data(repo: dict):
                 }
                 dateCreated
                 lastActivityAt
+                forkCount
                 license {
                     name
                     title
                 }
-    $contributions_fragment
-    $tracker_fragment
-            }
-        }
-    }
     """
-    )
+    
     # Query fragement for `contributions`
     contributions_fragement: string.Template = string.Template(
     """
-                contributions(after: $contributions_after, sortBy: "dateCreated") {
+                contributions(after: "$contributions_after", sortBy: "dateCreated") {
                     edges {
                         node {
                             id
@@ -165,14 +164,13 @@ def get_project_data(repo: dict):
     """
     )
     # Query fragment for `tracker`
-    tracker_fragment: string.Template = string.Template(
-    """
+    tracker_fragment: string.Template = string.Template("""
                 tracker {
-                    issues(after: $issues_after, sortBy: "dateCreated") {
+                    issues(after: "$issues_after", sortBy: "dateCreated") {
                         edges {
                             node {
                                 id
-                                issueSlug
+                                slug
                                 type
                                 dateCreated
                                 lastUpdated
@@ -198,7 +196,7 @@ def get_project_data(repo: dict):
     """
     )
 
-    print(f"Retrieving {repo['space']}/{repo['slug']}'s issues metadata...", file=sys.stderr)
+    print(f"Retrieving {repo['space']}/{repo['slug']}'s data...", file=sys.stderr)
 
     # Start with one query for project metadata, contributions, and issues. 
     # After that, loop through each to see if it `hasNextPage` then run 
@@ -209,13 +207,111 @@ def get_project_data(repo: dict):
     tracker_query: str = tracker_fragment.substitute(issues_after="")
     query: str = main_query.substitute(space=repo["space"],
                                        slug=repo["slug"],
-                                       contributions_fragement=contributions_query,
-                                       tracker_fragment=tracker_query)
+                                       project_metadata=project_metadata_fragment,
+                                       contributions=contributions_query,
+                                       tracker=tracker_query)
 
     # Make starting query
-    response = make_query(query=query).json()
+    response = make_query(query=query).json()["data"]["project"]["result"]
 
-    return response
+    # Process contributions from starting query
+    # First, get a list of query `id`s
+
+    # Save current results
+    project_metadata: dict = dict((key, response[key]) for key in ["title", 
+                                                                   "creator",
+                                                                   "dateCreated",
+                                                                   "id", 
+                                                                   "license", 
+                                                                   "forkCount"
+                                                                   ])
+    contribution_ids: list = list()
+    for edge in response["contributions"]["edges"]:
+        contributions.extend(edge["node"])
+        contribution_ids.extend(edge["node"]["id"])
+    issue_ids: list = list()
+    for edge in response["tracker"]["issues"]["edges"]:
+        issues.extend(edge["node"])
+        issue_ids.extend(edge["node"]["id"])
+    # Make more queries as long as there is `hasNextPage == True`
+    contributions_next_page: bool = response["contributions"]["pageInfo"]["hasNextPage"]
+    issues_next_page: bool = response["tracker"]["issues"]["pageInfo"]["hasNextPage"]
+    if contributions_next_page or issues_next_page:
+        # Track if there is a next page of results
+        query_has_next_page: bool = True
+        contributions_end_cursor: str = response["contributions"]["pageInfo"]["endCursor"]
+        issues_end_cursor: str = response["tracker"]["issues"]["pageInfo"]["endCursor"]
+        while query_has_next_page:
+            if contributions_next_page and issues_next_page:
+                print(f"There are more contributions and issues to query...")
+                # Query for more contributions and issues
+                contributions_query = contributions_fragement.substitute(contributions_after=contributions_end_cursor)
+                tracker_query = tracker_fragment.substitute(issues_after=issues_end_cursor)
+                query = main_query.substitute(space=repo["space"],
+                                              slug=repo["slug"],
+                                              project_metadata="",
+                                              contributions=contributions_query,
+                                              tracker=tracker_query)
+                response = make_query(query=query).json()["data"]["project"]["result"]
+                # Save new results
+                for edge in response["contributions"]["edges"]:
+                    contributions.extend(edge["node"])
+                    contribution_ids.extend(edge["node"]["id"])
+                for edge in response["tracker"]["issues"]["edges"]:
+                    issues.extend(edge["node"])
+                    issue_ids.extend(edge["node"]["id"])
+                # Update cursors and check if there's next page of results
+                contributions_end_cursor = response["contributions"]["pageInfo"]["endCursor"]
+                issues_end_cursor = response["tracker"]["issues"]["pageInfo"]["endCursor"]
+                contributions_next_page = response["contributions"]["pageInfo"]["hasNextPage"]
+                issues_next_page = response["tracker"]["issues"]["pageInfo"]["hasNextPage"]
+            elif contributions_next_page:
+                print(f"There are more contributions to query...")
+                # Query for more contributions
+                contributions_query = contributions_fragement.substitute(contributions_after=contributions_end_cursor)
+                query = main_query.substitute(space=repo["space"],
+                                              slug=repo["slug"],
+                                              project_metadata="",
+                                              contributions=contributions_query,
+                                              tracker="")
+                response = make_query(query=query).json()["data"]["project"]["result"]
+                # Save new results
+                for edge in response["contributions"]["edges"]:
+                    contributions.extend(edge["node"])
+                    contribution_ids.extend(edge["node"]["id"])
+                # Update cursors and check if there's next page of results
+                contributions_end_cursor = response["contributions"]["pageInfo"]["endCursor"]
+                contributions_next_page = response["contributions"]["pageInfo"]["hasNextPage"]
+            elif issues_next_page:
+                print(f"There are more issues to query...")
+                # Query for more issues
+                tracker_query = tracker_fragment.substitute(issues_after=issues_end_cursor)
+                query = main_query.substitute(space=repo["space"],
+                                              slug=repo["slug"],
+                                              project_metadata="",
+                                              contributions="",
+                                              tracker=tracker_query)
+                response = make_query(query=query).json()["data"]["project"]["result"]
+                # Save new results
+                for edge in response["tracker"]["issues"]["edges"]:
+                    issues.extend(edge["node"])
+                    issue_ids.extend(edge["node"]["id"])
+                # Update cursors and check if there's next page of results
+                issues_end_cursor = response["tracker"]["issues"]["pageInfo"]["endCursor"]
+                issues_next_page = response["tracker"]["issues"]["pageInfo"]["hasNextPage"]
+            else:
+                print(f"Done with queries")
+                query_has_next_page = False
+    
+    print(f"There are {len(contribution_ids)} contributions and {len(issue_ids)} issues in this project", file=sys.stderr)
+    
+    # Combined data
+    combined_data: dict = {
+        "Repository": project_metadata,
+        "Commits": contributions,
+        "Tickets": issues
+    }
+    return combined_data
 
 
 def Wikifactory(repo_list: pandas.core.frame.DataFrame) -> dict:
@@ -263,7 +359,7 @@ def Wikifactory(repo_list: pandas.core.frame.DataFrame) -> dict:
         # Query for the data now
         if repo_error == False: 
             try:
-                response_data = get_project_data(repo=repo_url_components)
+                response_data: dict = get_project_data(repo=repo_url_components)
                 repo["issues"] = None
                 repo["commits"] = None
             except WikifactoryAPIError:
