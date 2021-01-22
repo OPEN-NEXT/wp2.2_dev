60861442bdf049787754078b59c59a8b828cb49d
Pen-Yuan Hsing <penyuanhsing@posteo.is>
2021-01-22 18:49:41 +0000

    Export mined data to JSON compressed in ZIP archive

--- osdash/postprocess/exporter.py
+++ osdash/postprocess/exporter.py
@@ -4,13 +4,24 @@
 
 # Python Standard Library imports
 import json
+import zipfile
+
+# Filename of mined data file that will be compressed
+DATA_FILE: str = "mined_data.json"
 
 def save_mined_data(path: str, mined_data: list):
     """
     docstring
     """
     # Export mined data as JSON file
-    # TODO: Consider compressing the output to save disk space???
+
+    # Save output JSON file in a compressed archive with `zipfile` module
+    with zipfile.ZipFile(path, mode="w", compression=zipfile.ZIP_DEFLATED) as zip_file: 
+        dumped_JSON: str = json.dumps(mined_data, ensure_ascii=False, indent=4)
+        zip_file.writestr(DATA_FILE, data=dumped_JSON)
+        # Test integrity of compressed archive
+        zip_file.testzip()
     
-    with open(file=path, mode="w", encoding="utf-8") as output_file:
-        json.dump(mined_data, output_file, ensure_ascii=False, indent=4)
+    # Just save the JSON file with no compression
+    # with open(file=path, mode="w", encoding="utf-8") as output_file:
+    #     json.dump(mined_data, output_file, ensure_ascii=False, indent=4)
