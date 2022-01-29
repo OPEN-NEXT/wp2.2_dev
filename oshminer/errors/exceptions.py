94dcab6d32e8833a3c348e0db1fd304ca7437c16
Pen-Yuan Hsing <penyuan@users.noreply.github.com>
2022-01-29 14:16:18 +0000

    Add a module for custom exceptions

---
+++ oshminer/errors/exceptions.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2022 Pen-Yuan Hsing
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+class BadRepoError(Exception): 
+    pass
