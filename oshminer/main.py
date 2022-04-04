cef26d097c76f1d278659bb990b1f81da1d9500f
Pen-Yuan Hsing <penyuan@users.noreply.github.com>
2022-04-04 17:25:26 +0100

    Change query type to POST
    Changed from GET to POST per request from Wikifactory.

--- oshminer/main.py
+++ oshminer/main.py
@@ -55,7 +55,7 @@ async def process_repo(repo: HttpUrl, requests: list[str], responses: list):
         )
     responses.append(repo_info)
 
-@app.get(
+@app.post(
     "/data/", 
     name = "API endpoint", 
     description = "Primary endpoint for requesting data."
