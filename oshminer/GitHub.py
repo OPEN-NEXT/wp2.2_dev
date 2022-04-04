f4408a6cfd53167ede8093a76ab255861964f21f
Pen-Yuan Hsing <penyuan@users.noreply.github.com>
2022-04-04 22:10:05 +0100

    Fix GitHub handler to use async

--- oshminer/GitHub.py
+++ oshminer/GitHub.py
@@ -3,13 +3,13 @@
 # SPDX-FileCopyrightText: 2022 Pen-Yuan Hsing
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-def make_GitHub_request(url: str, data: list) -> str: 
+async def make_GitHub_request(url: str, data: list) -> dict: 
     print(f"Constructing an API request to GitHub for repository {url} for the following data {data}")
 
     # Create a dictionary to hold results from GitHub API query
     results: dict = {
         "repository": str(url), 
-        "platform": "Wikifactory", 
+        "platform": "GitHub", 
         "requested_data": {}
     }
 
